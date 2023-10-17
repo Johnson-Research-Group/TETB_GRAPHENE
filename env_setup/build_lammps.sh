d76b33a572afd429890848c3e726899bd20ed132
dpalmer-anl <dpalmer3@illinois.edu>
2023-10-17 13:10:48 -0500

    add documentation, adjust how interface calls potential files, add lammps build script

---
+++ env_setup/build_lammps.sh
@@ -0,0 +1,16 @@
+
+$LAMMPS_DIR = ${HOME}
+cwd = pwd
+$POTENTIAL_FILES = cwd/TEGT/parameters
+source activate $CONDA_ENV
+cd $LAMMPS_DIR; git clone https://github.com/lammps/lammps.git
+cd lammps; git checkout b8acd2e31de3d19ac0a4629ef168e183335bcc74
+
+cd src; cp $POTENTIAL_FILES/pair_reg_dep_poly.* .
+make clean-all;
+make yes-tally;
+make yes-molecule;
+make yes-manybody;
+make yes-interlayer;
+make -j20 mode=shared serial;
+make install-python
