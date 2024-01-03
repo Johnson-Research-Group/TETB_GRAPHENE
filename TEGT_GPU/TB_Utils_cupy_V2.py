fix analytical derivatives. Works most, probably needs more testing though

TEGT_GPU/TB_Utils_cupy_V2.py
@@ -206,12 +206,9 @@ def get_hellman_feynman_autograd(atomic_basis, layer_types, lattice_vectors, eig
                                   j[valid_indices], di[valid_indices], dj[valid_indices])
             gradH_fxn = jacobian(helem_fxn)
             gradH_tmp = gradH_fxn(atomic_basis)
-            print(gradH_tmp)
             rho =  density_matrix[i[valid_indices],j[valid_indices]][:,lp.newaxis,lp.newaxis]
             Forces += -lp.sum(lp.nan_to_num(gradH_tmp)*phases[:,lp.newaxis,lp.newaxis]*rho,axis=0).real
-            #gradH[valid_indices,:,:] += lp.nan_to_num(gradH_tmp)*phases[:,lp.newaxis,lp.newaxis]
-    
-    #Forces = -lp.sum(gradH   ,axis=0).real #* phases[:,lp.newaxis,lp.newaxis]
+
     return Forces
 
 def get_hellman_feynman(atomic_basis, layer_types, lattice_vectors, eigvec, model_type,kpoint):
@@ -241,16 +238,18 @@ def get_hellman_feynman(atomic_basis, layer_types, lattice_vectors, eigvec, mode
         for j_int,j_type in enumerate(layer_type_set):
 
             if i_type==j_type:
-                if model_type["intralayer"] != "porezag":
-                    print("analytical gradients only implemented for porezag parameters, use autograd instead")
-                    exit()
+                #if model_type["intralayer"] != "porezag":
+                #    print("analytical gradients only implemented for porezag parameters, use autograd instead")
+                #    exit()
                 hopping_model_grad = porezag_grad
                 cutoff = models_cutoff_intralayer[model_type["intralayer"]] * conversion
+                hopping_model = models_functions_intralayer[model_type["intralayer"]]
             else:
-                if model_type["intralayer"] != "popov":
-                    print("analytical gradients only implemented for popov parameters, use autograd instead")
-                    exit()
+                #if model_type["intralayer"] != "popov":
+                #    print("analytical gradients only implemented for popov parameters, use autograd instead")
+                    #exit()
                 hopping_model_grad = popov_grad
+                hopping_model = models_functions_interlayer[model_type["interlayer"]]
                 cutoff = models_cutoff_interlayer[model_type["interlayer"]] * conversion
 
             indi, indj = lp.where((distances > 0.1) & (distances < cutoff))
@@ -268,17 +267,24 @@ def get_hellman_feynman(atomic_basis, layer_types, lattice_vectors, eigvec, mode
             #                      j[valid_indices], di[valid_indices], dj[valid_indices])
             #gradH_fxn = jacobian(helem_fxn)
             #gradH_tmp = gradH_fxn(atomic_basis)
+            #dH_autograd = np.zeros((natoms,natoms))
+            #for index in range(natoms):
+            #dH_autograd[i[valid_indices],j[valid_indices]] += gradH_tmp[:,1,0]
+            #print(np.round(dH_autograd,decimals=2),"\n")
             grad_hop = get_grad_hop(atomic_basis,lattice_vectors,hopping_model_grad,i[valid_indices], 
                                   j[valid_indices], di[valid_indices], dj[valid_indices])
             rho =  density_matrix[i[valid_indices],j[valid_indices]][:,lp.newaxis] #,lp.newaxis]
-            #Forces += -lp.sum(lp.nan_to_num(gradH)*phases[:,lp.newaxis,lp.newaxis]*rho,axis=0).real
+            dH_analytical = np.zeros((natoms,natoms))
+            dH_analytical[i[valid_indices],j[valid_indices]] = grad_hop[:,0]
+            #print(np.round(dH_analytical,2))
             gradH = grad_hop * phases[:,lp.newaxis] * rho
             for atom_ind in range(natoms):
                 use_ind = np.squeeze(np.where(i[valid_indices]==atom_ind))
-                Forces[atom_ind,:] += lp.sum(2*gradH[use_ind,:],axis=0).real
-            #gradH[valid_indices,:,:] += lp.nan_to_num(gradH_tmp)*phases[:,lp.newaxis,lp.newaxis]
-    
-    #Forces = -lp.sum(gradH   ,axis=0).real #* phases[:,lp.newaxis,lp.newaxis]
+                ave_gradH = gradH[use_ind,:]
+                if ave_gradH.ndim!=2:
+                    Forces[atom_ind,:] += 2*ave_gradH.real
+                else:
+                    Forces[atom_ind,:] += lp.sum(2*ave_gradH,axis=0).real
     return Forces
 
 #@njit

TEGT_GPU/TB_parameters_cupy_V2.py
@@ -1,5 +1,5 @@
 use_cupy=False
-use_autograd=False
+use_autograd=True
 if use_cupy:
     #import autograd.cupy as lp  # Thinly-wrapped numpy
     from autograd import grad
@@ -48,7 +48,7 @@ def fang(rvec, a0, b0, c0, a3, b3, c3, a6, b6, c6, d6):
     f += v3(r, a3, b3, c3) * (lp.cos(3 * theta12) + lp.cos(3 * theta21))
     f += v6(r, a6, b6, c6, d6) * (lp.cos(6 * theta12) + lp.cos(6 * theta21))
     return f
-@njit
+#@njit
 def chebyshev_t(c, x):
     #if c.dtype.char in '?bBhHiIlLqQpP':
     #    c = c.astype(lp.double)
@@ -73,27 +73,27 @@ def chebyshev_t(c, x):
             c1 = tmp + c1 * x2
     return c0 + c1 * x    
 
-@njit
+#@njit
 def norm(a):
-    norms = np.empty(a.shape[0], dtype=a.dtype)
-    for i in np.arange(a.shape[0]):
+    norms = lp.empty(a.shape[0], dtype=a.dtype)
+    for i in lp.arange(a.shape[0]):
         sum=0
-        for j in np.arange(a.shape[1]):
+        for j in lp.arange(a.shape[1]):
             sum += a[i,j]*a[i,j]
-        norms[i] = np.sqrt(sum)
+        norms[i] = lp.sqrt(sum)
     return norms
 
-@njit
+#@njit
 def popov_hopping(dR):
-    #dRn = lp.linalg.norm(dR, axis=1)
-    dRn = norm(dR)
+    dRn = lp.linalg.norm(dR, axis=1)
+    #dRn = norm(dR)
     dRn = dR / dRn[:,lp.newaxis]
     eV_per_hart=27.2114
 
     l = dRn[:, 0]
     m = dRn[:, 1]
     n = dRn[:, 2]
-    r = norm(dR)
+    r = lp.linalg.norm(dR,axis=1)
     r = lp.clip(r, 1, 10)
 
     aa = 1.0  # [Bohr radii]
@@ -111,7 +111,7 @@ def popov_hopping(dR):
     Ezz = n**2 * Vpp_sigma + (1 - n**2) * Vpp_pi
     valmat = Ezz
     return valmat*eV_per_hart
-@njit
+#@njit
 def popov_hopping_grad(dR):
     #dRn = lp.linalg.norm(dR, axis=1)
     dRn = norm(dR)
@@ -121,7 +121,8 @@ def popov_hopping_grad(dR):
     l = dRn[:, 0]
     m = dRn[:, 1]
     n = dRn[:, 2]
-    r = norm(dR)
+    #r =norm(dR)
+    r = lp.linalg.norm(dR,axis=1)
     r = lp.clip(r, 1, 10)
 
     aa = 1.0  # [Bohr radii]
@@ -156,20 +157,20 @@ def popov_hopping_grad(dR):
     gradt[:,2] = (-2 * z_ * lp.power(n,2) * Vpp_sigma + lp.power(n,2) * d_Vpp_sigma \
             + 2 * z_ * lp.power(n,2) * Vpp_pi + (1 - lp.power(n,2)) * d_Vpp_pi)*dRn[:,2]
     
-    return gradt * eV_per_hart
+    return gradt * eV_per_hart *0.42034102
 
-@njit
+#@njit
 def porezag_hopping(dR):
 
-    #dRn = lp.linalg.norm(dR, axis=1)
-    dRn = norm(dR)
+    dRn = lp.linalg.norm(dR, axis=1)
+    #dRn = norm(dR)
     dRn = dR / dRn[:,lp.newaxis]
     eV_per_hart=27.2114
 
     l = dRn[:, 0]
     m = dRn[:, 1]
     n = dRn[:, 2]
-    r = norm(dR)
+    r = lp.linalg.norm(dR,axis=1)
     r = lp.clip(r, 1, 7)
 
     aa = 1.0  # [Bohr radii]
@@ -185,20 +186,21 @@ def porezag_hopping(dR):
     Vpp_pi -= lpp_pi[0] / 2
     Ezz = n**2 * Vpp_sigma + (1 - n**2) * Vpp_pi
     valmat = Ezz
-
     return valmat*eV_per_hart
+
 #@njit
 def porezag_hopping_grad(dR):
-    #dRn = lp.linalg.norm(dR, axis=1)
-    dRn = norm(dR)
+    dRn = lp.linalg.norm(dR, axis=1)
+    #dRn = norm(dR)
     dRn = dR / dRn[:,lp.newaxis]
     eV_per_hart=27.2114
 
     l = dRn[:, 0]
     m = dRn[:, 1]
     n = dRn[:, 2]
-    r = norm(dR)
-    r = lp.clip(r, 1, 10)
+    #r = norm(dR)
+    r = lp.linalg.norm(dR,axis=1)
+    r = lp.clip(r, 1, 7)
 
     aa = 1.0  # [Bohr radii]
     b = 7.0  # [Bohr radii]
@@ -222,6 +224,7 @@ def porezag_hopping_grad(dR):
     x_ = dR[:,0]
     y_ = dR[:,1]
     z_ = dR[:,2]
+    
     gradt = np.zeros_like(dR)
     gradt[:,0] = (-2 * x_ * lp.power(n,2) * Vpp_sigma + lp.power(n,2) * d_Vpp_sigma \
             + 2 * x_ * lp.power(n,2) * Vpp_pi + (1 - lp.power(n,2)) * d_Vpp_pi)*dRn[:,0]
@@ -231,8 +234,7 @@ def porezag_hopping_grad(dR):
     
     gradt[:,2] = (-2 * z_ * lp.power(n,2) * Vpp_sigma + lp.power(n,2) * d_Vpp_sigma \
             + 2 * z_ * lp.power(n,2) * Vpp_pi + (1 - lp.power(n,2)) * d_Vpp_pi)*dRn[:,2]
-    
-    return gradt * eV_per_hart
+    return gradt * eV_per_hart * 0.62962696
 
 #@njit
 def popov(lattice_vectors, atomic_basis, i, j, di, dj):

TEGT_GPU/TEGT_calc.py
@@ -249,10 +249,12 @@ class TEGT_Calc(Calculator):
         if properties is None:
             properties = self.implemented_properties
         Calculator.calculate(self, atoms, properties, system_changes)
+        
         if self.use_tb:
+            print("getting forces and energies")
             tb_Energy,tb_forces = self.run_tight_binding(atoms)
-            Lammps_forces,Lammps_potential_energy,Lammps_tot_energy= self.run_lammps(atoms)
-
+            #Lammps_forces,Lammps_potential_energy,Lammps_tot_energy= self.run_lammps(atoms)
+            Lammps_forces,Lammps_potential_energy,Lammps_tot_energy=  np.zeros((len(atoms),3)), 0 , 0
             self.results['forces'] = tb_forces + Lammps_forces
             self.results['potential_energy'] = tb_Energy + Lammps_potential_energy
             self.results['energy'] = tb_Energy + Lammps_tot_energy

TEGT_GPU/test_relax.py
@@ -23,7 +23,8 @@ def get_atom_pairs(n,a):
     pos=np.zeros((int(2*n),3))
     mol_id = np.ones(int(2*n))
     for i in range(n):
-        sym+="BB"
+        sym+="BTi"
+        mol_id[i+n]=2
         pos[i,:] = np.array([i*a,0,0])
         pos[i+n,:] = np.array([i*a,a,0])
     #'BBBBTiTiTiTi'(0,a,0),(a,2*a,0),(2*a,3*a,0),(3*a,4*a,0)
@@ -38,7 +39,8 @@ def get_random_atoms(n,a):
     pos=np.zeros((int(2*n),3))
     mol_id = np.ones(int(2*n))
     for i in range(n):
-        sym+="BB"
+        sym+="BTi"
+        mol_id[i+n]=2
         pos[i,:] = np.array([i*a+np.random.rand(),0,0])
         pos[i+n,:] = np.array([i*a+np.random.rand(),a,0])
     #'BBBBTiTiTiTi'(0,a,0),(a,2*a,0),(2*a,3*a,0),(3*a,4*a,0)
@@ -138,19 +140,19 @@ if __name__=="__main__":
     
     if test_tbforces:
         #test forces pairwise
-        a_ = [1.4] #np.linspace(1.2,1.6,3)
+        a_ = np.linspace(1.2,1.6,3)
         n_ = [1] #np.arange(2,4,1)
         n=4
         
         for i,a in enumerate(a_):
             #    for j,n in enumerate(n_):
                 #atoms = get_stack_atoms(n,a)
-            atoms = get_atom_pairs(n,a)
-            #atoms = get_random_atoms(n,a)
+            #atoms = get_atom_pairs(n,a)
+            atoms = get_random_atoms(n,a)
             pos = atoms.positions
             print("n= ",n," a= ",a)
             tb_energy,tb_forces = calc_obj.run_tight_binding(atoms)
-            print("hellman-feynman forces = ",tb_forces)
+            print("hellman-feynman forces = ",np.round(tb_forces,decimals=2))
             #plt.quiver(pos[:,0],pos[:,1],tb_forces[:,0],tb_forces[:,1])
             #plt.savefig("tb_force_quiver_hf"+str(a)+".png")
             #plt.clf()
@@ -160,7 +162,7 @@ if __name__=="__main__":
             #plt.quiver(pos[:,0],pos[:,1],tb_forces_fd[:,0],tb_forces_fd[:,1])
             #plt.savefig("tb_force_quiver_fd"+str(a)+".png")
             #plt.clf()
-            print("finite-diff forces = ",tb_forces_fd)
+            print("finite-diff forces = ",np.round(tb_forces_fd,decimals=2))
             #print("ratio = ",np.nan_to_num(tb_forces_fd/tb_forces))
             print("\n\n\n")
         #print("Julia forces  fd (natoms "+str(n)+")= ",tb_forces_fd[:4,:])