140718ddc92e4544dcb6cdb5bed8010b6c9b965f
dpalmer-anl <66490877+dpalmer-anl@users.noreply.github.com>
2024-04-12 10:40:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="TETB_GRAPHENE",
     version="0.1",
+    author="Daniel Palmer, Naheed Ferdous, Gabriel Brown, Tawfiqur Rakib,Kittithat Krongchon, Lucas K. Wagner, and Harley T. Johnson",
+    author_email="dpalmer3@illinois.edu",
     packages=find_packages(),
-    install_requires=[
-        # Your package dependencies
-    ],
+    install_requires=[joblib,dask,dask_cuda,ase,h5py,pandas,cupy],
     include_package_data=True,
 )
 
